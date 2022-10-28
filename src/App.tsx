a3d6774877a302c15ba3d701e816839e82380845
Jessica Shin <jessica.shin@pdgc.com>
2022-10-28 15:07:31 -0700

    Create react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div>
+      <img src={logo} alt="logo" className="w-8 inline-block" />
+      Hello World
+    </div>
+  );
+}
+
+export default App;
